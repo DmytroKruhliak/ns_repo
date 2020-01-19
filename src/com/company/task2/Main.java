0ab3c61e495260fab2347caa5c741c9bff417536
Kruhliak_Dmytro <46688347+KruhliakDmytro@users.noreply.github.com>
2020-01-19 15:25:25 +0200

    init rep

---
+++ src/com/company/task2/Main.java
@@ -0,0 +1,40 @@
+package com.company.task2;
+
+import interfaces.task2.FractionNumber;
+
+public class Main implements FractionNumber {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    @Override
+    public void setDividend(int i) {
+
+    }
+
+    @Override
+    public int getDividend() {
+        return 0;
+    }
+
+    @Override
+    public void setDivisor(int i) {
+
+    }
+
+    @Override
+    public int getDivisor() {
+        return 0;
+    }
+
+    @Override
+    public double value() {
+        return 0;
+    }
+
+    @Override
+    public String toStringValue() {
+        return null;
+    }
+}
